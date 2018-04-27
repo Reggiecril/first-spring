848836f5f283e14d3fc87f9aad54c084f083c190
Reggiecril <reggiecril0618@gmail.com>
2018-04-27 14:44:03 +0100

    CRUD is available

--- src/main/java/com/reggie/guestbook/domain/GuestBookEntry.java
+++ src/main/java/com/reggie/guestbook/domain/GuestBookEntry.java
@@ -1,19 +1,47 @@
 package com.reggie.guestbook.domain;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Entity
+@Table(name = "entries")
 public class GuestBookEntry {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column (name = "ID")
     private Integer id;
+    @Size(min=2,max=10)
+    @Column (name = "USER")
     @NotEmpty
     private String user;
     @NotEmpty
+    @Size(min=2,max=10)
+    @Column (name = "COMMENT")
     private String comment;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
 }
--- src/main/java/com/reggie/guestbook/service/GuestBookService.java
+++ src/main/java/com/reggie/guestbook/service/GuestBookService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+
 @Service
 public class GuestBookService {
     @Autowired
@@ -15,5 +16,19 @@ public class GuestBookService {
     public List<GuestBookEntry> findAllEntries () {
         return this.guestBookEntryRepository.findAll ();
     }
-
+    public GuestBookEntry findGuestBookEntryById (Integer id) {
+        return this.guestBookEntryRepository.findGuestBookEntryById(id);
+    }
+    public List <GuestBookEntry> findGuestBookEntryByUser (String user) {
+        return this.guestBookEntryRepository.findGuestBookEntryByUser (user);
+    }
+    public void deleteGuestBookEntryById (Integer id) {
+        this.guestBookEntryRepository.delete(guestBookEntryRepository.findGuestBookEntryById(id));
+    }
+    public void save (GuestBookEntry newEntry) {
+        this.guestBookEntryRepository.save(newEntry);
+    }
+    public GuestBookEntry findOne (Integer id) {
+        return this.guestBookEntryRepository.findGuestBookEntryById(id);
+    }
 }
